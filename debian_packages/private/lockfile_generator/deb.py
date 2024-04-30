72f606edaa82b9f4fd5fb20feee3617a12bfbab3
Hunter Crosland <hcrosland@nvidia.com>
2024-04-30 22:17:55 +0000

    better URL partitioing in exact sources

--- debian_packages/private/lockfile_generator/deb.py
+++ debian_packages/private/lockfile_generator/deb.py
@@ -292,13 +292,13 @@ class PackageIndexGroup:
         )
 
     def _exact_package_index(self, source: str, index: int) -> PackageIndex:
-        pool_root_url_, _, rest = source.partition("/dists/")
+        pool_root_url_, _, rest = source.partition("dists/")
         return PackageIndex(
             name=f"exact_{str(index)}",
             arch=self.arch,
             distro=self.distro,
             pool_root_url=pool_root_url_,
-            index_file_path= f"dists/{rest}/binary-{self.debian_arch}/Packages.xz",
+            index_file_path= f"dists/{rest}" + '/' if rest[-1] != '/' else '' + "binary-{self.debian_arch}/Packages.xz",
         )
 
     def resolve_package(
